19b61715a56b0711792299792187b59ea0a60d2e
Gokul avinash <gokulvmrf@gmail.com>
2026-04-19 21:42:23 +0530

    Implement NLTK data download checks
    Added NLTK data download checks for tokenizers and stopwords.

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except:
+    nltk.download('stopwords')
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
+stop_words = set(stopwords.words('english'))
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
@@ -21,7 +32,7 @@ def transform_text(text):
     y.clear()
 
     for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
+        if i not in stop_words and i not in string.punctuation:
             y.append(i)
 
     text = y[:]
